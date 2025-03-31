fffd20e8e39ad2b57ba9a7211f2c4f1ba1a17c0b
Guimeb <rm551981@fiap.com.br>
2025-03-31 20:02:59 -0300

    Alterando estrutura e estilização

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -6,73 +6,100 @@ import quizz_questions from "../../../assets/data/quizz_questions.json"
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-
 export class QuizzComponent implements OnInit {
 
-  title:string = ""
+  title: string = "";
 
-  questions:any
-  questionSelected:any
+  questions: any;
+  questionSelected: any;
 
-  answers:string[] = []
-  answerSelected:string =""
+  answers: string[] = [];
+  answerSelected: string = "";
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  finished:boolean = false
+  finished: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+      this.questions.forEach((question: any) => {
+        question.options = this.shuffleArray(question.options);
+      });
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
 
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
-    }
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+      this.questionSelected = this.questions[this.questionIndex];
 
-  }
-
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
-  }
-
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      console.log(this.questionIndex);
+      console.log(this.questionMaxIndex);
     }
   }
 
-  async checkResult(anwsers:string[]){
+  shuffleArray(array: any[]): any[] {
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
-
-    return result
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
 
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+    }
+  }
+
+  async checkResult(answers: string[]): Promise<string> {
+    let frequency: { [key: string]: number } = {};
+
+    answers.forEach((answer, index) => {
+      const weight = 1;
+      frequency[answer] = (frequency[answer] || 0) + weight;
+    });
+
+    let maxScore = 0;
+    for (let key in frequency) {
+      if (frequency[key] > maxScore) {
+        maxScore = frequency[key];
+      }
+    }
+
+    let potentialResults: string[] = [];
+    for (let key in frequency) {
+      if (frequency[key] === maxScore) {
+        potentialResults.push(key);
+      }
+    }
+
+    if (potentialResults.length > 1) {
+      const randomIndex = Math.floor(Math.random() * potentialResults.length);
+      return potentialResults[randomIndex];
+    }
+
+    return potentialResults[0];
+  }
 }
